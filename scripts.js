a55f0b27d19bfd0a03addcb7c52ef45fa4514aa5
ancientmelody <ndavidkelly@gmail.com>
2021-02-19 20:43:00 -0500

    created namespace app + API fetch call

--- scripts.js
+++ scripts.js
@@ -6,3 +6,25 @@
 // Limit the return to maximum 5 strains
 // Display the recommended strain results from the API on the page by manipulating the DOM - include the strain name, description, and a visual aspect (picture or GIF)
 // Listen for a 'click' event on a reset button
+
+// Create app object
+const flavourTown = {};
+
+// Save API info
+flavourTown.apiUrl = 'https://strainapi.evanbusse.com/rpCJEMW/strains/search/flavor/FLAVOR';
+
+// Create method to request info from API
+flavourTown.getFlavours = () => {
+    fetch(flavourTown.apiUrl)
+        .then((response) => {
+            // Parse the response into JSON, return it so it can be used in the next function
+            return response.json();
+        })
+            // First then returns a Promise, parse the JSON response and log readable data
+            .then((jsonResponse) => {
+                console.log(jsonResponse);
+            });
+};
+
+// Call the getFlavours method
+flavourTown.getFlavours();
