573286883c0509dded5d54f16684eae3ea5c875c
ancientmelody <ndavidkelly@gmail.com>
2021-02-19 20:11:22 -0500

    basic HTML structure + JS pseudo-code

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// A dropdown menu that allows the user to select a flavor of cannabis which has been pulled from the API
+// Listen for the user's click on the 'submit' button that will confirm their selection
+// Capture the value of the user's selection & store the user's selection in a variable
+// On submission, make an AJAX call to retrieve cannabis strains that match the user's selection 
+// Loop through the strains returned by the API 
+// Limit the return to maximum 5 strains
+// Display the recommended strain results from the API on the page by manipulating the DOM - include the strain name, description, and a visual aspect (picture or GIF)
+// Listen for a 'click' event on a reset button
