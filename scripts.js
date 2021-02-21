d50756988a8f553654a43993cc2e9c69994f9997
rosie <rosieontheinternet@gmail.com>
2021-02-21 18:02:31 -0500

    adding the list to the page

--- scripts.js
+++ scripts.js
@@ -25,12 +25,33 @@ flavourTown.getStrains = (flavour) => {
 // Method to loop through strains and display on the page
 flavourTown.displayStrains = (jsonResults) => {
     console.log(jsonResults.length);
+
+    // const ul = document.querySelector('ul');
+
     jsonResults.forEach(item => {
         console.log(`${item.name}, ${item.race}`);
-        // Get title and race and display on page
-        // Display hard-coded GIF relating to flavour
+        //create list element to store title & race
+        const listEl = document.createElement('li');
+
+        //create h4 to hold title 
+        const strainTitle = document.createElement('h4');
+        strainTitle.textContent = item.name
+
+        //create p to hold race
+        const race = document.createElement('p');
+        race.textContent = item.race;
+
+        document.querySelector('ul').appendChild(listEl);
+        listEl.appendChild(strainTitle);
+        listEl.appendChild(race);
+
+
+
         // Filter results down to 6-12 options
+        // Clear html results 
     });
+        // Display hard-coded GIF relating to flavour
+
 };
 
 // Get the user choice from the dropdown menu
@@ -39,8 +60,35 @@ flavourTown.getSelectedValue = () => {
     const selection = document.getElementById('flavours');
     form.addEventListener('submit', function(event){
         event.preventDefault();
+        
         flavourTown.getStrains(selection.value);
+        if (selection.value === 'sweet'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`;
+        }else if(selection.value === 'earthy'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === 'vanilla'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === 'citrus'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === 'nutty'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === 'coffee'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === `woody`){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }else if(selection.value === 'flowery'){
+            const gifHolder = document.querySelector('.gifHolder');
+            gifHolder.innerHTML = `<iframe src="https://giphy.com/embed/1oKjCzdnatyyMxkdxF" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>` 
+        }
     });
+
 };
 
 // Inti function
